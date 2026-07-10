f47334f8ce0f07ec79d4822cabbe9e6e8c504c65
sdabdullah <ab.faishal@gmail.com>
2026-07-10 20:19:35 +0600

    Matches table added in database

--- football-ticket-booking-query .sql
+++ football-ticket-booking-query .sql
@@ -13,3 +13,14 @@ CREATE TABLE Users (
     phone_number VARCHAR(14)
 );
 
+
+---- 2. Matches Table successfully Created in Beekeper
+CREATE TABLE Matches (
+    match_id SERIAL PRIMARY KEY,
+    fixture VARCHAR(80) NOT NULL,
+    tournament_category VARCHAR(50) NOT NULL,
+    base_ticket_price DECIMAL(10,2) NOT NULL CHECK(base_ticket_price > 0),
+    match_status VARCHAR(15) NOT NULL CHECK (match_status IN (
+        'Available','Selling Fast','Sold Out','Postponed'
+    ))
+);
