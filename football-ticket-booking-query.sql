d86c40383eb9184fba562c81938479ed7adea10b
sdabdullah <ab.faishal@gmail.com>
2026-07-10 23:12:21 +0600

    Query 5 added

--- football-ticket-booking-query.sql
+++ football-ticket-booking-query.sql
@@ -86,4 +86,12 @@ FROM
   JOIN users USING (user_id)
   JOIN matches USING (match_id)
 
-
+--- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+SELECT
+  user_id,
+  full_name,
+  booking_id
+FROM
+  users
+  LEFT JOIN bookings USING (user_id)
+  LEFT JOIN matches USING (match_id)
