3c46ec23de88914bca4d09ee78257ba5a604ddde
sdabdullah <ab.faishal@gmail.com>
2026-07-10 23:27:25 +0600

    Query 7 added

--- football-ticket-booking-query.sql
+++ football-ticket-booking-query.sql
@@ -112,3 +112,15 @@ WHERE
       bookings
   );
 
+--- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+
+SELECT
+  match_id,
+  fixture,
+  base_ticket_price
+FROM
+  matches
+ORDER BY
+  base_ticket_price DESC
+LIMIT (2)
+OFFSET (1)
